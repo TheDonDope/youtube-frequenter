c104515c4c45bb7fde3d11cef4449e149b4233d2
mn <mario.n@myronaxia.org>
2018-05-11 00:01:56 +0200

    epiphanie

--- go/service/exfoliate.go
+++ go/service/exfoliate.go
@@ -16,54 +16,56 @@ import (
 // - Playlists (With their PlaylistID and PlaylistName)
 // - SubscriberCount
 // - ViewCount
-func GetChannelInfo(service *youtube.Service, inChannel chan ChannelMetaInfo) chan ChannelMetaInfo {
+func GetChannelOverview(service *youtube.Service, inChannel chan ChannelMetaInfo) <-chan ChannelMetaInfo {
 
 	fmt.Println("GetChannelInfo Call")
 	outChannel := make(chan ChannelMetaInfo)
-	channelMetaInfo := <-inChannel
-	fmt.Println(fmt.Sprintf("GetChannelInfo gets: %v", channelMetaInfo))
-	call := service.Channels.List("contentDetails,snippet,statistics")
-	call = call.ForUsername(channelMetaInfo.CustomURL)
+	go func() {
+		channelMetaInfo := <-inChannel
+		fmt.Println(fmt.Sprintf("GetChannelInfo gets: %v", channelMetaInfo))
+		call := service.Channels.List("contentDetails,snippet,statistics")
+		call = call.ForUsername(channelMetaInfo.CustomURL)
 
-	response, responseError := call.Do()
-	HandleError(responseError, "GetChannelInfo Response error!")
+		response, responseError := call.Do()
+		HandleError(responseError, "GetChannelInfo Response error!")
 
-	firstItem := response.Items[0]
+		firstItem := response.Items[0]
 
-	// Fill ChannelID
-	if channelMetaInfo.ChannelID == "" {
-		channelMetaInfo.ChannelID = firstItem.Id
-		fmt.Println(channelMetaInfo.ChannelID)
-	}
+		// Fill ChannelID
+		if channelMetaInfo.ChannelID == "" {
+			channelMetaInfo.ChannelID = firstItem.Id
+			fmt.Println(channelMetaInfo.ChannelID)
+		}
 
-	// Fill CustomURL
-	if channelMetaInfo.CustomURL == "" {
-		channelMetaInfo.CustomURL = firstItem.Snippet.CustomUrl
-	}
+		// Fill CustomURL
+		if channelMetaInfo.CustomURL == "" {
+			channelMetaInfo.CustomURL = firstItem.Snippet.CustomUrl
+		}
 
-	// Fill ChannelName
-	if channelMetaInfo.ChannelName == "" {
-		channelMetaInfo.ChannelName = firstItem.Snippet.Title
-	}
+		// Fill ChannelName
+		if channelMetaInfo.ChannelName == "" {
+			channelMetaInfo.ChannelName = firstItem.Snippet.Title
+		}
 
-	// Fill Playlists
-	if channelMetaInfo.Playlists == nil {
-		channelMetaInfo.Playlists = append(channelMetaInfo.Playlists, Playlist{firstItem.ContentDetails.RelatedPlaylists.Uploads, "uploads", nil})
-		channelMetaInfo.Playlists = append(channelMetaInfo.Playlists, Playlist{firstItem.ContentDetails.RelatedPlaylists.Favorites, "favorites", nil})
-	}
+		// Fill Playlists
+		if channelMetaInfo.Playlists == nil {
+			channelMetaInfo.Playlists = append(channelMetaInfo.Playlists, Playlist{firstItem.ContentDetails.RelatedPlaylists.Uploads, "uploads", nil})
+			channelMetaInfo.Playlists = append(channelMetaInfo.Playlists, Playlist{firstItem.ContentDetails.RelatedPlaylists.Favorites, "favorites", nil})
+		}
 
-	// Fill SubscriberCount
-	if channelMetaInfo.SubscriberCount == 0 {
-		channelMetaInfo.SubscriberCount = firstItem.Statistics.SubscriberCount
-	}
+		// Fill SubscriberCount
+		if channelMetaInfo.SubscriberCount == 0 {
+			channelMetaInfo.SubscriberCount = firstItem.Statistics.SubscriberCount
+		}
 
-	// Fill ViewCount
-	if channelMetaInfo.ViewCount == 0 {
-		channelMetaInfo.ViewCount = firstItem.Statistics.ViewCount
-	}
-	fmt.Println(channelMetaInfo)
-	outChannel <- channelMetaInfo
-	fmt.Println("would return now.")
+		// Fill ViewCount
+		if channelMetaInfo.ViewCount == 0 {
+			channelMetaInfo.ViewCount = firstItem.Statistics.ViewCount
+		}
+		fmt.Println(channelMetaInfo)
+		outChannel <- channelMetaInfo
+		fmt.Println("would return now.")
+	}()
 	return outChannel
 }
 
@@ -74,12 +76,12 @@ func Exfoliator(service *youtube.Service, channelMetaInfo ChannelMetaInfo) Chann
 	go func() {
 		initialInChannel <- channelMetaInfo
 	}()
-
+	getChannelInfoOutChannel := GetChannelOverview(service, initialInChannel)
 	timeout := time.After(5 * time.Second)
+	// time.Sleep(time.Second)
 	select {
-	case channelMetaInfoFromChannel := <-GetChannelInfo(service, initialInChannel):
-		fmt.Println(fmt.Sprintf("Exfoliator gets: %v", channelMetaInfoFromChannel))
-		channelMetaInfo = channelMetaInfoFromChannel
+	case channelMetaInfo = <-getChannelInfoOutChannel:
+		fmt.Println(fmt.Sprintf("Exfoliator gets: %v", channelMetaInfo))
 	case <-timeout:
 		fmt.Println("timed out")
 		return channelMetaInfo
