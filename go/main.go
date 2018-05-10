de262e4017af16da355c21682b3ee74451f900ed
TheDonDope <idontreallyneedthese@gmail.com>
2018-05-10 18:58:06 +0200

    feat: cli with arguments

--- go/main.go
+++ go/main.go
@@ -2,21 +2,39 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
+
+	"github.com/jessevdk/go-flags"
 
 	"github.com/TheDonDope/youtube-tinfoil-expose/go/service"
 )
 
+var opts struct {
+	ChannelID string `short:"i" long:"channel-id" description:"The channel ID of a YouTube Channel"`
+
+	CustomURL string `short:"u" long:"custom-url" description:"The custom URL of a YouTube Channel"`
+
+	PlaylistName string `short:"n" long:"playlist-name" description:"The name of the playlist to search. Currently only supports the following playlists: uploads, favorites"`
+}
+
 func main() {
 	start := time.Now()
 	fmt.Println("Welcome to youtube-tinfoil-expose")
+
+	_, argsError := flags.ParseArgs(&opts, os.Args)
+
+	if argsError != nil {
+		panic(argsError)
+	}
+
 	youtubeService, serviceError := service.GetYouTubeService()
 
 	service.HandleError(serviceError, "Error creating YouTube client")
 
 	query := service.Query{}
-	query.CustomURL = "wwwKenFMde"
-	query.PlaylistName = "uploads"
+	query.CustomURL = opts.CustomURL
+	query.PlaylistName = opts.PlaylistName
 
 	results := service.Exfoliator(youtubeService, query)
 	fmt.Println(results)
