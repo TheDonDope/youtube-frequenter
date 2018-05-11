43e67471cfc8d85f8946cef4cca4bba138ea2aae
mn <mario.n@myronaxia.org>
2018-05-11 14:10:13 +0200

    prints removed

--- go/service/exfoliate.go
+++ go/service/exfoliate.go
@@ -33,19 +33,19 @@ func GetChannelOverview(service *youtube.Service, inChannel chan ChannelMetaInfo
 		// Fill ChannelID
 		if channelMetaInfo.ChannelID == "" {
 			channelMetaInfo.ChannelID = firstItem.Id
-			fmt.Println(fmt.Sprintf("channelMetaInfo.ChannelID: %s", channelMetaInfo.ChannelID))
+			//fmt.Println(fmt.Sprintf("channelMetaInfo.ChannelID: %s", channelMetaInfo.ChannelID))
 		}
 
 		// Fill CustomURL
 		if channelMetaInfo.CustomURL == "" {
 			channelMetaInfo.CustomURL = firstItem.Snippet.CustomUrl
-			fmt.Println(fmt.Sprintf("channelMetaInfo.CustomURL: %s", channelMetaInfo.CustomURL))
+			//fmt.Println(fmt.Sprintf("channelMetaInfo.CustomURL: %s", channelMetaInfo.CustomURL))
 		}
 
 		// Fill ChannelName
 		if channelMetaInfo.ChannelName == "" {
 			channelMetaInfo.ChannelName = firstItem.Snippet.Title
-			fmt.Println(fmt.Sprintf("channelMetaInfo.ChannelName: %s", channelMetaInfo.ChannelName))
+			//fmt.Println(fmt.Sprintf("channelMetaInfo.ChannelName: %s", channelMetaInfo.ChannelName))
 		}
 
 		// Fill Playlists
@@ -53,19 +53,19 @@ func GetChannelOverview(service *youtube.Service, inChannel chan ChannelMetaInfo
 			channelMetaInfo.Playlists = make(map[string]Playlist)
 			channelMetaInfo.Playlists["uploads"] = Playlist{firstItem.ContentDetails.RelatedPlaylists.Uploads, []Video{}}
 			channelMetaInfo.Playlists["favorites"] = Playlist{firstItem.ContentDetails.RelatedPlaylists.Favorites, []Video{}}
-			fmt.Println(fmt.Sprintf("channelMetaInfo.Playlists: %+v", channelMetaInfo.Playlists))
+			//fmt.Println(fmt.Sprintf("channelMetaInfo.Playlists: %+v", channelMetaInfo.Playlists))
 		}
 
 		// Fill SubscriberCount
 		if channelMetaInfo.SubscriberCount == 0 {
 			channelMetaInfo.SubscriberCount = firstItem.Statistics.SubscriberCount
-			fmt.Println(fmt.Sprintf("channelMetaInfo.SubscriberCount: %d", channelMetaInfo.SubscriberCount))
+			//fmt.Println(fmt.Sprintf("channelMetaInfo.SubscriberCount: %d", channelMetaInfo.SubscriberCount))
 		}
 
 		// Fill ViewCount
 		if channelMetaInfo.ViewCount == 0 {
 			channelMetaInfo.ViewCount = firstItem.Statistics.ViewCount
-			fmt.Println(fmt.Sprintf("channelMetaInfo.ViewCount: %d", channelMetaInfo.ViewCount))
+			//fmt.Println(fmt.Sprintf("channelMetaInfo.ViewCount: %d", channelMetaInfo.ViewCount))
 		}
 		fmt.Println(fmt.Sprintf("GetChannelOverview filling complete. Result: %+v", channelMetaInfo))
 		fmt.Println("Sending result to getChannelOverviewOutChannel...")
@@ -95,12 +95,12 @@ func GetVideoIDsOverview(service *youtube.Service, inChannel <-chan ChannelMetaI
 		for _, item := range response.Items {
 			video := Video{VideoID: item.Id}
 			videos = append(videos, video)
-			fmt.Println(fmt.Sprintf("Appended video %s to playlist uploads", video))
+			//fmt.Println(fmt.Sprintf("Appended video %s to playlist uploads", video))
 		}
 		uploadPlaylist := channelMetaInfo.Playlists["uploads"]
 		uploadPlaylist.PlaylistItems = videos
-		fmt.Println(fmt.Sprintf("uploadPlaylist.PlaylistItems now: %+v", uploadPlaylist.PlaylistItems))
-		fmt.Println(fmt.Sprintf("GetVideoIDsOverview filling complete. Result: %+v", channelMetaInfo))
+		//fmt.Println(fmt.Sprintf("uploadPlaylist.PlaylistItems now: %+v", uploadPlaylist.PlaylistItems))
+		//fmt.Println(fmt.Sprintf("GetVideoIDsOverview filling complete. Result: %+v", channelMetaInfo))
 		fmt.Println("Sending result to getVideoIDsOverviewOutChannel...")
 		outChannel <- channelMetaInfo
 		fmt.Println("Result successfully sent to getVideoIDsOverviewOutChannel")
@@ -125,12 +125,16 @@ func GetCommentsOverview(service *youtube.Service, inChannel <-chan ChannelMetaI
 			response, responseError := call.Do()
 			HandleError(responseError, "GetCommentsOverview Response error!")
 
+			var comments []Comment
+			fmt.Println(fmt.Sprintf("!!!!!!!!!!!!!!!!!!!!!!"))
 			for _, item := range response.Items {
 				comment := Comment{CommentID: item.Snippet.TopLevelComment.Id, AuthorChannelID: item.Snippet.TopLevelComment.Snippet.AuthorChannelId.(string)}
-				videoInput.Comments = append(videoInput.Comments, comment)
+				comments = append(comments, comment)
 				fmt.Println(fmt.Sprintf("Appended comment: %v to video: %v", comment, videoInput))
 				fmt.Println(fmt.Sprintf("videoInput.Comments now: %+v", videoInput.Comments))
 			}
+
+			videoInput.Comments = comments
 			fmt.Println(fmt.Sprintf("GetCommentsOverview filling complete. Result: %+v", channelMetaInfo))
 			fmt.Println("Sending result to getCommentsOverviewOutChannel...")
 			outChannel <- channelMetaInfo
