9cbbef180c0594aa583812b939aa6e74c116f9d7
TheDonDope <idontreallyneedthese@gmail.com>
2018-05-11 14:36:01 +0200

    docs: updated main logging

--- go/main.go
+++ go/main.go
@@ -20,26 +20,20 @@ var opts struct {
 
 func main() {
 	start := time.Now()
-	fmt.Println(fmt.Sprintf("Starting youtube-tinfoil-expose @ %v", start))
+	fmt.Println(fmt.Sprintf("Starting youtube-tinfoil-expose @ %v", start.Format(time.RFC3339)))
 
 	_, argsError := flags.ParseArgs(&opts, os.Args)
-
 	if argsError != nil {
 		panic(argsError)
 	}
 
 	youtubeService, serviceError := service.GetYouTubeService()
-
 	service.HandleError(serviceError, "Error creating YouTube client")
-
 	channelMetaInfo := service.ChannelMetaInfo{}
 	channelMetaInfo.CustomURL = opts.CustomURL
 	fmt.Println(fmt.Sprintf("Initial input channelMetaInfo: %+v", channelMetaInfo))
-
-	fmt.Println(fmt.Sprintf("%v ms have passed until now", time.Since(start)))
-
 	results := service.Exfoliator(youtubeService, channelMetaInfo)
 	fmt.Println(fmt.Sprintf("Exfoliator exfoliated successfully. Results: %+v", results))
 	elapsed := time.Since(start)
-	fmt.Println(fmt.Sprintf("Finishing youtube-tinfoil-expose @ %v. Overall time spent: %v ms.", time.Now(), elapsed))
+	fmt.Println(fmt.Sprintf("Finishing youtube-tinfoil-expose @ %v. Overall time spent: %v ms.", time.Now().Format(time.RFC3339), elapsed))
 }
