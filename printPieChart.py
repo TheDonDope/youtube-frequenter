bfa90b05484679223e8d1ea558490d9eeb74f5fe
mn <mario.n@myronaxia.org>
2018-05-12 22:48:43 +0200

    python pie chart nochmal

---
+++ printPieChart.py
@@ -0,0 +1,27 @@
+import json
+import matplotlib.pyplot as plt
+
+def draw_piechart(inc_list):
+    draw_list = []
+    labels = ()
+    sizes = []
+    max = 15
+    if len(inc_list) > max:
+        draw_list = inc_list[:max]
+    else:
+        draw_list = inc_list
+    for elem in draw_list:
+        labels = labels + (elem['key']+' '+str(elem['value']),)
+        sizes.append(elem['value'])
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+            shadow=False, startangle=90)
+    ax1.axis('equal')
+    plt.show()
+
+
+if __name__ == '__main__':
+    sorted_results = []
+    with open('results.json') as f:
+      sorted_results = json.load(f)
+    draw_piechart(sorted_results)
