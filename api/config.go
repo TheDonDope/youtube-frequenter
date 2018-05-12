6b15e7faa8670a930ddfb3c0aa187130cffc2ccf
TheDonDope <idontreallyneedthese@gmail.com>
2018-05-12 20:11:18 +0200

    feat: Global Timeout 100 sec

--- api/config.go
+++ api/config.go
@@ -8,15 +8,16 @@ const (
 	MaxResultsUploadedVideos = 50
 
 	// MaxResultsCommentPerVideo is the maximum search result number for the comments per video to be searched
-	MaxResultsCommentPerVideo = 10
+	MaxResultsCommentPerVideo = 20
 
 	// MaxResultsFavouritedVideos is the maximum search result number for the number of playlist items in the favourited playlist
-	MaxResultsFavouritedVideos = 10
+	MaxResultsFavouritedVideos = 50
 
 	// AverageAPICallDuration is the duration we estimate to average for a single API call
-	AverageAPICallDuration = 8 * time.Millisecond
+	AverageAPICallDuration = 10 * time.Millisecond
 
 	// GlobalTimeout is the timeout for the complete program
-	GlobalTimeout = MaxResultsUploadedVideos * MaxResultsCommentPerVideo * MaxResultsFavouritedVideos * AverageAPICallDuration
-	//
+	GlobalTimeout = 100 * time.Second
+	//GlobalTimeout = MaxResultsUploadedVideos * MaxResultsCommentPerVideo * MaxResultsFavouritedVideos * AverageAPICallDuration
+
 )
