b64008e348781a3e2a89176c80945adb93bf4cea
TheDonDope <idontreallyneedthese@gmail.com>
2018-05-12 20:24:47 +0200

    feat: configure up

--- api/config.go
+++ api/config.go
@@ -4,13 +4,13 @@ import "time"
 
 const (
 
-	// MaxResultsUploadedVideos is the maximum search result number for the initial PlaylistItems.List call for the uploaded playlist
+	// MaxResultsUploadedVideos is the maximum search result number for the initial PlaylistItems.List call for the uploaded playlist (0 - 50, default if not set: 5)
 	MaxResultsUploadedVideos = 50
 
-	// MaxResultsCommentPerVideo is the maximum search result number for the comments per video to be searched
-	MaxResultsCommentPerVideo = 20
+	// MaxResultsCommentPerVideo is the maximum search result number for the comments per video to be searched (0 - 100, default if not set: 20)
+	MaxResultsCommentPerVideo = 100
 
-	// MaxResultsFavouritedVideos is the maximum search result number for the number of playlist items in the favourited playlist
+	// MaxResultsFavouritedVideos is the maximum search result number for the number of playlist items in the favourited playlist (0 - 50, default if not set: 5)
 	MaxResultsFavouritedVideos = 50
 
 	// AverageAPICallDuration is the duration we estimate to average for a single API call
