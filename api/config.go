f5cf3bdfed9d89de9e8202a6e3f57bb6561a705f
mn <mario.n@myronaxia.org>
2018-05-12 22:30:37 +0200

    result muss nicht mehr gelöscht werden

--- api/config.go
+++ api/config.go
@@ -5,10 +5,10 @@ import "time"
 const (
 
 	// MaxResultsUploadedVideos is the maximum search result number for the initial PlaylistItems.List call for the uploaded playlist (0 - 50, default if not set: 5)
-	MaxResultsUploadedVideos = 25
+	MaxResultsUploadedVideos = 50
 
 	// MaxResultsCommentPerVideo is the maximum search result number for the comments per video to be searched (0 - 100, default if not set: 20)
-	MaxResultsCommentPerVideo = 50
+	MaxResultsCommentPerVideo = 100
 
 	// MaxResultsFavouritedVideos is the maximum search result number for the number of playlist items in the favourited playlist (0 - 50, default if not set: 5)
 	MaxResultsFavouritedVideos = 50
@@ -17,7 +17,7 @@ const (
 	AverageAPICallDuration = 10 * time.Millisecond
 
 	// GlobalTimeout is the timeout for the complete program
-	GlobalTimeout = 60 * time.Second
+	GlobalTimeout = 5 * 60 * time.Second
 	//GlobalTimeout = MaxResultsUploadedVideos * MaxResultsCommentPerVideo * MaxResultsFavouritedVideos * AverageAPICallDuration
 
 )
