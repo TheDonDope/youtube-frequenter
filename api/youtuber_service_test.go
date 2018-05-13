21ddf6c068473bfeec37ee2c8e1279e348587f22
TheDonDope <idontreallyneedthese@gmail.com>
2018-05-13 19:25:38 +0200

    test: provide youtuber mock

--- api/youtuber_service_test.go
+++ api/youtuber_service_test.go
@@ -1,1 +1,21 @@
 package api
+
+import (
+	"testing"
+
+	"google.golang.org/api/youtube/v3"
+)
+
+func TestChannelsList(t *testing.T) {
+	service := YouTuberMock{}
+
+	expectedResponse := &youtube.ChannelListResponse{}
+
+	actualResponse, actualResponseError := service.ChannelsList(nil, "nil", "nil")
+	if actualResponseError != nil {
+		t.Fatal(actualResponseError)
+	}
+	if actualResponse.Etag != expectedResponse.Etag {
+		t.Errorf(GetFormattedFailMessage("YouTuber#ChannelsList", "expectedResponse", "actualResponse"))
+	}
+}
