23b30d7e28731c06734f40a08a072ec2b7b5a58d
mn <mario.n@myronaxia.org>
2018-05-16 19:31:22 +0200

    example pic from getmax

--- getMax.py
+++ getMax.py
@@ -9,16 +9,19 @@ useRandomResults = False
 # default U25, C25, F25, d10ms, t60s,
 channelID = 'UCCj956IF62FbT7Gouszaj9w'
 filePath = 'output/channel-id-' + channelID + \
-    '/channel-id-' + channelID+'-results.json'
-uValues = [12, 25, 50]
-cValues = [25, 50, 100]
-fValues = [12, 25, 50]
-# in seconds
-timeout = 30
+    '/channel-id-' + channelID + '-results.json'
 
-numOfTotalCalculations = len(uValues) * len(cValues) * len(fValues)
+# max values: u=50, c=100, f=50
+uValues = [5, 12, 25, 38, 50]
+cValues = [5, 12, 25, 50, 100]
+fValues = [5, 12, 25, 38, 50]
+
+# in seconds
+timeout = 5
+
+numOfTotalCalc = len(uValues) * len(cValues) * len(fValues)
 timeoutPerCalculation = timeout
-numOfCurrentCalculation = 1
+numOfCurrentCalc = 1
 resulting3DArray = []
 bestRun = ''
 highestCount = 1
@@ -26,37 +29,50 @@ highestCount = 1
 for U in uValues:
     for C in cValues:
         for F in fValues:
-            print('['+str(numOfCurrentCalculation)+'/'+str(numOfTotalCalculations)+']')
             if useRandomResults:
                 R = random.randint(0, 10000)
                 if R > highestCount:
+                    bestrun = 'U:'+str(U)+'C:'+str(C)+'F:'+str(F)
                     highestCount = R
                 resulting3DArray.append((U, C, F, R))
             else:
-                commandline = 'go run ./cmd/youtube-frequenter/main.go -c '+channelID+' -U ' + \
-                    str(U)+' -C '+str(C)+' -F '+str(F)+' -t ' + \
-                    str(timeoutPerCalculation)+'s > /dev/null'
-                print(commandline)
+                # start youtube-frequenter
+                binaryPath = './cmd/youtube-frequenter/main.go'
+                arguments = '-c '+channelID+' ' +\
+                            '-U '+str(U)+' ' +\
+                            '-C '+str(C)+' ' +\
+                            '-F '+str(F)+' ' +\
+                            '-t '+str(timeoutPerCalculation)+'s'
+                commandline = 'go run '+binaryPath+' '+arguments+' > /dev/null'
                 os.system(commandline)
+
+                # and get the results
                 sorted_results = []
                 with open(filePath) as f:
                     sorted_results = json.load(f)
                 resultCount = len(sorted_results)
-                resulting3DArray.append((U, C, F, resultCount))
+                resulting3DArray.append((U, C, F, resultCount, sorted_results))
+
+                # and get the best parameter configuration
                 if resultCount > highestCount:
                     highestCount = resultCount
                     bestRun = commandline
-                print("resultcount:"+str(resultCount)+' ,highest resultcount:'+str(highestCount))
-            numOfCurrentCalculation += 1
+
+                # and print what happened since whole cycle
+                countPrefix = '['+str(numOfCurrentCalc) + \
+                    '/'+str(numOfTotalCalc)+']'
+                print(countPrefix+' '+commandline+'=> #channels:' + str(resultCount) +
+                      '(highest:'+str(highestCount)+')')
+            numOfCurrentCalc += 1
 
 
 print('bestRun:' + bestRun)
 fig = matplotlib.pyplot.figure()
 ax = fig.add_subplot(111, projection='3d')
-for u, c, f, r in resulting3DArray:
-  if r > 0:
-    ax.scatter(u, c, f, s=(1.*r)/highestCount*1000, color='g',
-               alpha=(1.*r) / highestCount)
+for u, c, f, r, sr in resulting3DArray:
+    if r > 0:
+        ax.scatter(u, c, f, s=(1.*r)/highestCount*800, color=[(1.*u/50, 1.*c/100, 1.*f/50)],
+                   alpha=(1.*r) / highestCount)
 ax.set_xlabel('max uploads')
 ax.set_ylabel('max comments')
 ax.set_zlabel('max favorite')
