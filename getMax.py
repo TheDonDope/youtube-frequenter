b6df2d7b96519a63f4c9a8e2cd0ee528527336b2
mn <mario.n@myronaxia.org>
2018-05-16 21:27:19 +0200

    mhwhatever

--- getMax.py
+++ getMax.py
@@ -1,23 +1,47 @@
 import os
 import json
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
 import random
+from mpl_toolkits.mplot3d import Axes3D
+import math
+from printPieChart import draw_piechart
 
 useRandomResults = False
 
 # default U25, C25, F25, d10ms, t60s,
-channelID = 'UCCj956IF62FbT7Gouszaj9w'
-filePath = 'output/channel-id-' + channelID + \
-    '/channel-id-' + channelID + '-results.json'
+customURL = 'wwwKenFMde'
+channelID = ''
+if channelID != '':
+    filePath = 'output/channel-id-' + channelID + \
+               '/channel-id-' + channelID + '-results.json'
+else:
+    filePath = 'output/custom-url-' + customURL + \
+               '/custom-url-' + customURL + '-results.json'
+
+
+# timeout which terminates each youtube-frequenter process
+# in seconds
+timeout = 10
 
 # max values: u=50, c=100, f=50
-uValues = [5, 12, 25, 38, 50]
-cValues = [5, 12, 25, 50, 100]
-fValues = [5, 12, 25, 38, 50]
+uMax = 10
+cMax = 25
+fMax = 25
 
-# in seconds
-timeout = 5
+uValues = []
+cValues = []
+fValues = []
+
+for u in range(1, uMax, int(uMax/3.)):
+    uValues.append(u)
+
+for c in range(1, cMax, int(cMax/3.)):
+    cValues.append(c)
+
+for f in range(1, fMax, int(fMax/3.)):
+    fValues.append(f)
+
+dataPointSize = 500
 
 numOfTotalCalc = len(uValues) * len(cValues) * len(fValues)
 timeoutPerCalculation = timeout
@@ -25,24 +49,43 @@ numOfCurrentCalc = 1
 resulting3DArray = []
 bestRun = ''
 highestCount = 1
+bestSorted_results = []
 
+# search through valid parameter configurations
 for U in uValues:
     for C in cValues:
         for F in fValues:
+            resultCount = 0
+            commandline = ''
             if useRandomResults:
-                R = random.randint(0, 10000)
-                if R > highestCount:
-                    bestrun = 'U:'+str(U)+'C:'+str(C)+'F:'+str(F)
-                    highestCount = R
-                resulting3DArray.append((U, C, F, R))
+                # fake commandline
+                commandline = 'U:' + str(U) + 'C:' + str(C) + 'F:' + str(F)
+
+                # get results ''for commandline''
+                resultCount = random.randint(0, 10000)
+
+                # save best random configuration
+                if resultCount > highestCount:
+                    bestrun = commandline
+                    highestCount = resultCount
+
+                    bestSorted_results = [{'key': 'channel1', 'value': 200},
+                                          {'key': 'channel2', 'value': 50}]
+                resulting3DArray.append((U, C, F, resultCount, ['a']))
             else:
                 # start youtube-frequenter
                 binaryPath = './cmd/youtube-frequenter/main.go'
-                arguments = '-c '+channelID+' ' +\
-                            '-U '+str(U)+' ' +\
-                            '-C '+str(C)+' ' +\
-                            '-F '+str(F)+' ' +\
-                            '-t '+str(timeoutPerCalculation)+'s'
+                target = ''
+                if channelID != '':
+                    target = '-c ' + channelID
+                else:
+                    target = '-u ' + customURL
+
+                arguments = target + ' ' +\
+                    '-U '+str(U)+' ' +\
+                    '-C '+str(C)+' ' +\
+                    '-F '+str(F)+' ' +\
+                    '-t '+str(timeoutPerCalculation)+'s'
                 commandline = 'go run '+binaryPath+' '+arguments+' > /dev/null'
                 os.system(commandline)
 
@@ -51,29 +94,41 @@ for U in uValues:
                 with open(filePath) as f:
                     sorted_results = json.load(f)
                 resultCount = len(sorted_results)
-                resulting3DArray.append((U, C, F, resultCount, sorted_results))
 
                 # and get the best parameter configuration
                 if resultCount > highestCount:
                     highestCount = resultCount
                     bestRun = commandline
+                    bestSorted_results = sorted_results
 
-                # and print what happened since whole cycle
-                countPrefix = '['+str(numOfCurrentCalc) + \
-                    '/'+str(numOfTotalCalc)+']'
-                print(countPrefix+' '+commandline+'=> #channels:' + str(resultCount) +
-                      '(highest:'+str(highestCount)+')')
+                resulting3DArray.append((U, C, F, resultCount, sorted_results))
+            # and print what happened since whole cycle
+            countPrefix = '['+str(numOfCurrentCalc) + \
+                '/'+str(numOfTotalCalc)+']'
+
+            print(countPrefix+' '+commandline+'=> #channels:' + str(resultCount) +
+                  '(highest:'+str(highestCount)+')')
             numOfCurrentCalc += 1
 
+if __name__ == '__main__':
 
-print('bestRun:' + bestRun)
-fig = matplotlib.pyplot.figure()
-ax = fig.add_subplot(111, projection='3d')
-for u, c, f, r, sr in resulting3DArray:
-    if r > 0:
-        ax.scatter(u, c, f, s=(1.*r)/highestCount*800, color=[(1.*u/50, 1.*c/100, 1.*f/50)],
-                   alpha=(1.*r) / highestCount)
-ax.set_xlabel('max uploads')
-ax.set_ylabel('max comments')
-ax.set_zlabel('max favorite')
-show()
+    print('bestRun:' + bestRun)
+    fig = plt.figure()
+    ax = fig.add_subplot(211, projection='3d')
+
+    # add every datapoint to 3d-plot
+    for u, c, f, r, sr in resulting3DArray:
+        if r > 0:
+            ax.scatter(u, c, f, s=(1.*r)/highestCount*dataPointSize, color=[(1.*u/50, 1.*c/100, 1.*f/50)],
+                       alpha=(1. * r) / highestCount)
+
+    # set axis names
+    ax.set_xlabel('max uploads')
+    ax.set_ylabel('max comments')
+    ax.set_zlabel('max favorite')
+
+    # append piechart with pos=212(?)
+    draw_piechart(fig, bestSorted_results)
+
+    # view graphic
+    plt.show()
--- printPieChart.py
+++ printPieChart.py
@@ -1,22 +1,26 @@
 import json
-import matplotlib.pyplot as plt
 import sys
+import matplotlib.pyplot as plt
 
 
-def draw_piechart(inc_list):
+
+def draw_piechart(fig, inc_list):
 
     draw_list = []
     labels = ()
     sizes = []
     max = 15
+
+    # only use the heighest #max elements
     if len(inc_list) > max:
         draw_list = inc_list[:max]
     else:
         draw_list = inc_list
+
     for elem in draw_list:
         labels = labels + (elem['key']+' '+str(elem['value']),)
         sizes.append(elem['value'])
-    _, ax1 = plt.subplots()
+    ax1 = fig.add_subplot(212)
     ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
             shadow=False, startangle=90)
     ax1.axis('equal')
